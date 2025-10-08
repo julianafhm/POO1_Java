649894227d3a53b872bd4f763cac40beb3279b86
Jmelo <Jmelo@PrillNotebook18.prill.local>
2025-10-07 22:23:25 -0300

    Melhorias no código

--- RevisaoAv1/src/revisaoav1/Tipo.java
+++ RevisaoAv1/src/revisaoav1/Tipo.java
@@ -1,9 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package revisaoav1;
 
+package revisaoav1;
 import java.util.Scanner;
  
 public class Tipo extends Calcado{
