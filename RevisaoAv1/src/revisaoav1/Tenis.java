4caee9574739e09f2d039e7fb52c8f4625238fb5
Jmelo <Jmelo@PrillNotebook18.prill.local>
2025-10-07 22:10:27 -0300

    Melhorias no código

--- RevisaoAv1/src/revisaoav1/Tenis.java
+++ RevisaoAv1/src/revisaoav1/Tenis.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package revisaoav1;
 
 import java.util.Scanner;
@@ -22,9 +18,9 @@ public class Tenis extends Tipo{
     public void imprimir(){
         super.imprimir();
         if (esportivo){
-            System.out.println("É tenis esportivo");
+            System.out.println("O tenis é esportivo");
         }  else{
-        System.out.println("não é tenis esportivo");
+        System.out.println("O tenis não é tenis esportivo");
         }
     }
     public void entrada(){
